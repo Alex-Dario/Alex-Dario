ecc39c94a02f99f4a9f1c87f4b1833db7192a01f
Alex-Dario <138732243+Alex-Dario@users.noreply.github.com>
2023-10-16 21:46:13 -0300

    Add files via upload

---
+++ Desafio001/D001.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto</title>
+    <link rel="stylesheet" href="style1.css">
+
+</head>
+
+<body>
+    <header>Controle de Ordem</header>
+    <table>
+        <thead>
+            <tr>
+                <td>Bags</td>
+                <td>Set</td>
+                <td>Real</td>
+                <td>OP</td>
+                <td>Desvio</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Bag1</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Bag2</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Bag3</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Bag4</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+            <tr>
+                <td>Bag5</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+                <td>0</td>
+            </tr>
+        </tbody>
+    </table>
+    <form action="cad.php" method="get">
+        <input type="submit" value="Bag 1">
+        <input type="submit" value="Bag 2">
+        <input type="submit" value="Bag 3">
+        <input type="submit" value="Bag 4">
+        <input type="submit" value="Bag 5">
+    </form>
+</body>
+
+</html>
